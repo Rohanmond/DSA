14d41b39b904d8192afe073a7ecc383c9e3a40aa
Rohan Mondal <mondalrohan108@gmail.com>
2021-07-28 12:45:04 +0530

    4th

--- CIP CB/Heaps/joinTheRopes.cpp
+++ CIP CB/Heaps/joinTheRopes.cpp
@@ -4,21 +4,13 @@ int joinRopes(vector<int> &v)
 {
     priority_queue<int, vector<int>, greater<int>> pq(v.begin(), v.end());
     int res = 0;
-
-    while (true)
+    while (pq.size() > 1)
     {
-        //     if (pq.empty())
-        //         break;
         int a = pq.top();
         pq.pop();
-        if (pq.empty())
-            break;
         int b = pq.top();
         pq.pop();
         res += (a + b);
-        cout << (a + b) << endl;
-        if (pq.empty())
-            break;
         pq.push(a + b);
     }
     return res;
